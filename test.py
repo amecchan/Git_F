709ca37b10f621fb4f32db9dda911b8e06afc7fc
Amecchan <rodette30@gmail.com>
2025-04-29 14:54:06 +0800

    Implement even/odd number checker program

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hello world")
+#A program that will tell you if a number is even or odd.
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+        print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
