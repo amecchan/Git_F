dde83bedda0e6a342b52a1ef3c6f1f75bd2ff35b
Amecchan <rodette30@gmail.com>
2025-05-07 19:12:04 +0800

    added algorithm description

--- algo/bub-sort.py
+++ algo/bub-sort.py
@@ -2,6 +2,9 @@
 toys = [5, 3, 1, 4, 2]
 
 for i in range(len(toys)):
+    print(f"Iteration {i + 1}: {toys}")
+    # Bubble sort algorithm
+    # Compare adjacent/close elements and swap if they are in the wrong order.
     for j in range(len(toys) - 1):
         if toys[j] > toys[j + 1]:
             toys[j], toys[j + 1] = toys[j + 1], toys[j]
