aa05ea8ae6b0f64343d19ce77d2317f71c3df2e0
Amecchan <rodette30@gmail.com>
2025-05-07 19:20:07 +0800

    refactor: clean up comments and enhance print statements in bubble sort

--- algo/bub-sort.py
+++ algo/bub-sort.py
@@ -1,4 +1,4 @@
-# main branch
+
 toys = [5, 3, 1, 4, 2]
 
 for i in range(len(toys)):
@@ -6,7 +6,10 @@ for i in range(len(toys)):
     # Bubble sort algorithm
     # Compare adjacent/close elements and swap if they are in the wrong order.
     for j in range(len(toys) - 1):
+        print(toys, "\n\nSwapping", toys[j], "and", toys[j + 1])
+        # Compare adjacent elements and swap if they are in the wrong order
         if toys[j] > toys[j + 1]:
+            # Swap the elements
             toys[j], toys[j + 1] = toys[j + 1], toys[j]
 
 print(toys)
