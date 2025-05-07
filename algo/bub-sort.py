94a86830eff8be3f129ef8e76c8d2e3c9881c48a
Amecchan <rodette30@gmail.com>
2025-05-07 19:16:10 +0800

    improve output formatting in bubble algorithm for better readability

--- algo/bub-sort.py
+++ algo/bub-sort.py
@@ -2,11 +2,15 @@
 toys = [5, 3, 1, 4, 2]
 
 for i in range(len(toys)):
-    print(f"Iteration {i + 1}: {toys}")
+    print(f"\nIteration {i + 1}: {toys}")
     # Bubble sort algorithm
     # Compare adjacent/close elements and swap if they are in the wrong order.
     for j in range(len(toys) - 1):
+        print(toys, "\nComparing", toys[j], "and", toys[j + 1])
+        # Compare the current element with the next one
+        # If the current element is greater than the next one, swap them
         if toys[j] > toys[j + 1]:
+            # Swap the elements
             toys[j], toys[j + 1] = toys[j + 1], toys[j]
 
 print(toys)
