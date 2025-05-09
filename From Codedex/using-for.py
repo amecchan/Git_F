12ac0dedf0cbdfd444fa22064b72bbd389a39296
Amecchan <rodette30@gmail.com>
2025-05-09 15:21:41 +0800

    add Detention program to print message 100 times

---
+++ From Codedex/using-for.py
@@ -0,0 +1,11 @@
+# Suppose you got detention 
+# and the teacher wants you to write "I will not use Snapchat in class" on the whiteboard 100 times.
+# Create a detention.py program that does this using code.
+
+# This is where we begin to see the true power of computing. 
+# What takes humans hours can take a computer microseconds.
+
+for i in range(100):
+    print("I will not use Snapchat in class.")
+# The for loop is a control flow statement that allows code to be executed repeatedly.
+# The range function generates a sequence of numbers, which is used to control the number of iterations.
